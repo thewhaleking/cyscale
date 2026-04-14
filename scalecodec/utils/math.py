1f3626b0da07e66225ba820e2f71333f36bcbceb
BD Himes <b@latent.to>
2026-04-14 20:38:54 +0200

    Import the cython math utils

--- scalecodec/utils/math.py
+++ scalecodec/utils/math.py
@@ -6,12 +6,35 @@ from decimal import Decimal
 from math import ceil, log2
 from typing import TypedDict
 
+try:
+    from scalecodec.utils._math import trailing_zeros, next_power_of_two
+except ImportError:
+
+    def trailing_zeros(value: int) -> int:
+        """Returns the number of trailing zeros in the binary representation of
+        the given integer.
+        """
+        num_zeros = 0
+        while value & 1 == 0:
+            num_zeros += 1
+            value >>= 1
+        return num_zeros
+
+    def next_power_of_two(value: int) -> int:
+        """Returns the smallest power of two that is greater than or equal
+        to the given integer.
+        """
+        if value < 0:
+            raise ValueError("Negative integers not supported")
+        return 1 if value == 0 else 1 << ceil(log2(value))
+
 
 class FixedPoint(TypedDict):
     """
     Represents a fixed point `U64F64` number.
     Where `bits` is a U128 representation of the fixed point number.
     """
+
     bits: int
 
 
@@ -76,23 +99,3 @@ def fixed_to_decimal(value: _FixedInput, frac_bits: int = 64) -> Decimal:
     integer_part = bits >> frac_bits
     fractional_part = bits & frac_mask
     return Decimal(integer_part) + Decimal(fractional_part) / Decimal(1 << frac_bits)
-
-
-def trailing_zeros(value: int) -> int:
-    """Returns the number of trailing zeros in the binary representation of
-    the given integer.
-    """
-    num_zeros = 0
-    while value & 1 == 0:
-        num_zeros += 1
-        value >>= 1
-    return num_zeros
-
-
-def next_power_of_two(value: int) -> int:
-    """Returns the smallest power of two that is greater than or equal
-    to the given integer.
-    """
-    if value < 0:
-        raise ValueError("Negative integers not supported")
-    return 1 if value == 0 else 1 << ceil(log2(value))
